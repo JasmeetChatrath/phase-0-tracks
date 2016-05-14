1b2288e08e757c62e0b289d5f2624086acd9a88a
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-05-13 18:24:45 -0700

    Add array_drill.md solution

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,30 @@
+
+array = Array.new
+array += [1, 2, 3, 4, 5]
+array.delete(3)
+array.insert(2, 7)
+array.shift
+p array
+
+array2 = [7, 8, 9, 10]
+new_array = array + array2
+p new_array
+
+
+def build_array(x, y, z)
+  array = [x, y, z]
+  p array
+end
+
+build_array(1, "two", nil)
+
+
+def add_to_array(x, y)
+  array = x << y
+  p array
+end
+
+add_to_array([], "a")
+add_to_array(["a", "b", "c", 1, 2], 3)
+
+
