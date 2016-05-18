624da14511a6bf1cbb21526547a7c580402c4491
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-05-18 11:24:35 -0700

    Add puppy.rb practice problems

---
+++ ruby/puppy.rb
@@ -0,0 +1,38 @@
+SPECIES-------
+puppious adorables
+
+CHARACTERISTICS-------
+Freindly: yes
+Eye count: 2
+Paw count: 4
+Name: varies
+Color: varies
+Fur length: varies
+
+BEHAVIOR--------
+Hoping
+Playing
+Fetching
+
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+spot.class
+duchess == fido
+fido.instance_of?(Array)
+fido.instance_of?(Puppy)
+spot.play_dead
+
+
+str = String.new
+str.length
+str.empty?
+str += "huh."
+greeting = String.new("hello")
+greeting.length
+greeting.upcase
