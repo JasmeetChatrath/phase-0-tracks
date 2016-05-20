4c1a007479046439220f94a826c49ebe312a0edc
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-05-19 20:12:47 -0700

    Add complete solution for puppy_methods.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,30 +1,69 @@
-
 class Puppy
+
   def initialize
-    puts "Initializing new puppy instance..."
+    puts "Initializing new puppy instance ..."
   end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-  def speak(integer)
-    integer.times {puts "Woof!"}
+
+  def speak(num)
+    num.times { puts "woof!"}
   end
+
   def roll_over
-    puts "*rolls over*"
+    puts "**roll over**"
   end
-  def dog_years(human_years)
-    puts human_years/7
+
+  def dog_years(years)
+    puts years / 7
   end
+
   def play_dead
-    puts "*plays dead*"
+    puts "**play dead**"
+  end
+
+end
+
+puppy = Puppy.new
+puppy.fetch("frisbee")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(49)
+puppy.play_dead
+
+
+class Cat
+  def initialize
+    puts "Initializing cat instance ..."
+  end
+  def meow
+    puts "meow meow"
+  end
+  def cat_jump(num)
+    puts "the cat jumps #{num} times"
   end
 end
 
-yogi = Puppy.new
-yogi.fetch("frisby")
-yogi.speak(3)
-yogi.roll_over
-yogi.dog_years(26)
-yogi.play_dead
+cat = Cat.new
+cat.meow
+cat.cat_jump(5)
+
+cat_data_structure = []
+i = 1
+while i < 51
+  cat1 = Cat.new
+  cat_data_structure << cat1
+  i += 1
+end
+
+
+cat_data_structure.each do |cat|
+  cat.meow
+  cat.cat_jump(3)
+end
+
+
 
