810793ab0d1dd9c3c254c1833324e9cda6b170bb
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-05-19 17:08:43 -0700

    Add release 0 practice to santa.rb

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+  end
+  def reindeer_ranking
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
+  def age
+    @age = 0
+  end
+end
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
