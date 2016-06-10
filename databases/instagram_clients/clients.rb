667274703828b5a5055edcc4fd966b3a574fec4c
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-06-10 15:37:40 -0700

    Add client database

--- databases/instagram_clients/clients.rb
+++ databases/instagram_clients/clients.rb
@@ -3,3 +3,15 @@ require 'faker'
 
 
 db = SQLite3::Database.new( "clients.db" )
+
+create_table_cmd = <<-SQL
+  CREATE TABLE clients(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    quote INT,
+    complete_date VARCHAR(255),
+    service_requested VARCHAR(255)
+    )
+SQL
+
+db.execute(create_table_cmd)
