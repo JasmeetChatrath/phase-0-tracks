1c7bc6ac43c536360e5737457bee3a49c233f59a
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-06-10 17:22:36 -0700

    Add user interaction to clients.rb

--- databases/instagram_clients/clients.rb
+++ databases/instagram_clients/clients.rb
@@ -17,10 +17,21 @@ SQL
 
 db.execute(create_table_cmd)
 
+
+puts "Who is your client?"
+client_name = gets.chomp
+puts "What service are you providing?"
+service_name = gets.chomp
+puts "How much will you charge for this service?"
+service_quote = gets.chomp.to_i
+puts "When do the need this service completed by?"
+date_of_completion = gets.chomp
+
 def create_client(db, name, quote, complete_date, service_requested)
   db.execute("INSERT INTO clients (name, quote, complete_date, service_requested) VALUES (?, ?, ?, ?)", [name, quote, complete_date, service_requested])
 end
 
+create_client(db, client_name, service_quote, date_of_completion, service_name)
 
 clients = db.execute("SELECT * FROM clients")
 clients.each do |client|
