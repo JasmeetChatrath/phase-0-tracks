148d2808373d4b0ecee075b6326789a6b1df3130
JasmeetChatrath <Jasmeet.Chatrath@gmail.com>
2016-06-10 17:16:29 -0700

    Add create client method

--- databases/instagram_clients/clients.rb
+++ databases/instagram_clients/clients.rb
@@ -3,9 +3,10 @@ require 'faker'
 
 
 db = SQLite3::Database.new( "clients.db" )
+db.results_as_hash = true
 
 create_table_cmd = <<-SQL
-  CREATE TABLE clients(
+  CREATE TABLE IF NOT EXISTS clients(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     quote INT,
@@ -15,3 +16,17 @@ create_table_cmd = <<-SQL
 SQL
 
 db.execute(create_table_cmd)
+
+def create_client(db, name, quote, complete_date, service_requested)
+  db.execute("INSERT INTO clients (name, quote, complete_date, service_requested) VALUES (?, ?, ?, ?)", [name, quote, complete_date, service_requested])
+end
+
+
+clients = db.execute("SELECT * FROM clients")
+clients.each do |client|
+  puts "#{client['name']} requested"
+  puts "#{client['service_requested']}"
+  puts "Quote: $#{client['quote']}"
+  puts "Deadline: #{client['complete_date']}."
+end
+
